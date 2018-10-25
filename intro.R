eabfae3b5368a9c2f18bda04cf09e5b13a1c6bb1
gladdy007 <gladsonvinay@gmail.com>
2018-10-25 14:15:47 +0530

    25

---
+++ intro.R
@@ -0,0 +1,36 @@
+#starting to work in R
+
+#assign-----
+x1=3
+x2<-3
+#print values
+(x3=3)
+#enviornmentt---
+#variables in env
+ls()
+#datasets available for use
+data()
+women
+data(women)
+?women
+?mtcars
+mtcars
+attach(mtcars)
+mpg
+#libraries currently loaded
+libary()
+#elements
+ls()
+rm(list=ls())
+rm(list=ls(all=TRUE))
+x1
+data(mtcars)
+
+#help
+?mean
+x<- c(0:10,50)
+xm<-mean(x)
+xm
+c(xm, mean(x, trim=0.10))
+help(mean)
+getwd()
